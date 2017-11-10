70c92a34d916236c34db279c4e8dd20c58bc085e
Dana Palazzo <danapalazzo@Danas-MacBook.local>
2017-11-10 09:33:06 -0500

    Created and added game board divs to container

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
 // ****************************************
 // * Game Board
 // ****************************************
+$(() => {
+
+for (let i = 0; i < 20; i++) {
+  const $boardDivs = $("<div>")
+  .addClass("board-div")
+  .appendTo(".main-board-container")
+  .insertAfter(".safe-place");
+};
+
+
+
+
+
+
+}); // Closes window.onload
