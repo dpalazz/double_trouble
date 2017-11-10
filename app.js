75e11a97b5dc7c4607fe076630146871216ddf3d
Dana Palazzo <danapalazzo@Danas-MacBook.local>
2017-11-10 16:00:58 -0500

    Created event listeners for how to play, start game, and popper

--- app.js
+++ app.js
@@ -16,10 +16,26 @@ $(() => {
   // *************************
   const openModal = () => {
     $modal.css('display', 'block');
-  }
+  };
+
   const closeModal = () => {
     $modal.css('display', 'none');
-  }
+  };
+
+  // *****************
+  // * For the Popper
+  // *****************
+  const startGame = () => {
+    $(".col-1").children().css("background-color", "yellow");
+    $(".col-6").children().css("background-color", "green");
+  };
+
+  // *****************
+  // * For the Popper
+  // *****************
+  const popPopper = () => {
+    $(".popper").css("background-color", "black");
+  };
 
   // **************************************************************************
   // * Event Listeners
@@ -28,15 +44,18 @@ $(() => {
   // ************************
   // * For Instruction Modal
   // ************************
-  $("button").on("click", openModal);
+  $("#openModal").on("click", openModal);
   $closeBtn.on('click', closeModal);
+
+  // ********
+  // * Start
+  // ********
+  $("#start").on("click", startGame);
 
   // *****************
   // * For the Popper
   // *****************
-  $(".popper").on("click", () => {
-    console.log("clicked!");
-  });
+  $(".popper").on("click", popPopper);
 
 
 
