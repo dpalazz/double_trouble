fdca6fdc4db13fa63745de7bfc4858838a9ecd7b
Dana Palazzo <danapalazzo@Danas-MacBook.local>
2017-11-10 10:25:36 -0500

    Rearranged game board with top middle and bottom columns for better visual

--- app.js
+++ app.js
@@ -3,13 +3,6 @@
 // ****************************************
 $(() => {
 
-for (let i = 0; i < 20; i++) {
-  const $boardDivs = $("<div>")
-  .addClass("board-div")
-  .appendTo(".main-board-container")
-  .insertAfter(".safe-place");
-};
-
 
 
 
